d1e1bf222fd5c806e0e3ebf71d1492e357ead243
Shujie Ma <shujie1st@gmail.com>
2023-02-21 12:04:32 -0500

    Setup a separate module for TCP connection

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  
+  // handle and print incoming data from server
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-  
-  // handle and print incoming data from server
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
