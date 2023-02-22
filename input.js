ce0360990e6bca2b89648cbb8102e2f23b44c10c
Shujie Ma <shujie1st@gmail.com>
2023-02-21 21:12:10 -0500

    Create the input module

---
+++ input.js
@@ -0,0 +1,19 @@
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // Handle "ctrl" + "c" input to terminate the game
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  // Handle "ctrl" + "c" input to terminate the game
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
 
 setupInput();
