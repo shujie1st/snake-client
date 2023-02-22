f5ac3b4f50dbff341a8e36b627c5e82269fbcbe0
Shujie Ma <shujie1st@gmail.com>
2023-02-21 20:54:43 -0500

    Setup stdin

--- play.js
+++ play.js
@@ -2,3 +2,23 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // Handle "ctrl" + "c" input to terminate the game
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
